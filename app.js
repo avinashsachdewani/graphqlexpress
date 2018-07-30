399e2aa71c986ef63a456a8f0a72eb3b81486ccd
INSEAD_ORG\A.SACHDEWANI <avinash.sachdewani@insead.edu>
2018-07-30 10:56:49 +0800

    Graphql todo app changes

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const app = express()
+const schema = require('./graphql/schema/Schema')
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+  }))
+app.get('/', (req, res) => res.send("Hello world !"))
+// run server on port 3000
+app.listen('3000', _ => console.log('Server is listening on port 3000…'))
